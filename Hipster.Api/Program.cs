a9c6ff8d7ab050b41046d1e9b3c3ee2571488bfe
Serhii Surnin <ssurnin@softserveinc.com>
2022-02-09 03:18:02 +0200

    Added swagger support for Open API

--- Hipster.Api/Program.cs
+++ Hipster.Api/Program.cs
@@ -1,7 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument(option => {
+    option.Title = "Hipster.Api";
+});
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseOpenApi();
+
+app.UseSwaggerUi3();
+
+// app.UseEndpoints(endpoints =>
+// {
+//     endpoints.MapControllers();
+// });
 
 app.MapControllers();
 
